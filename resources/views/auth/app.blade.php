6fa839dfe8637b09cd08ba0c66e86d456e83120c
Sannyaaa <hasangunners30@gmail.com>
2025-04-12 20:32:25 +0700

    fixing app

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -52,7 +52,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    @vite('assets/css/app.css','assets/js/app.js')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
     @stack('style')
   </head>
--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -27,7 +27,7 @@
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
 
-        @vite('assets/css/app.css')
+        @vite(['resources/css/app.css','resources/js/app.js'])
 
         @stack('style')
 
